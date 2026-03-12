c431e68a2e8e8346cfc1ace83c8f4e934ff0f7fd
Nihar Chopade <65650226+Bobya9595@users.noreply.github.com>
2026-03-12 12:19:29 +0530

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,61 +1,32 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "@/lib/firebase";
-import PublicHeader from "@/components/PublicHeader";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../../lib/firebase";
+import PublicHeader from "../../components/PublicHeader";
 
 export default function LoginPage() {
-  const router = useRouter();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = async (e: any) => {
-    e.preventDefault();
-    await signInWithEmailAndPassword(auth, email, password);
-    router.push("/dashboard");
+  const login = async () => {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider);
   };
 
   return (
-    <>
+    <div className="min-h-screen bg-[#020617] text-white">
+
       <PublicHeader />
 
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="w-[420px] bg-white p-10 rounded-2xl shadow-xl">
-          <h2 className="text-2xl font-bold mb-6 text-center">
-            Welcome Back
-          </h2>
+      <div className="flex items-center justify-center h-[80vh]">
 
-          <form onSubmit={handleLogin} className="space-y-5">
-            <input
-              type="email"
-              placeholder="Email"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full p-4 border rounded-xl"
-            />
+        <button
+          onClick={login}
+          className="bg-purple-600 px-6 py-3 rounded-lg"
+        >
+          Login with Google
+        </button>
 
-            <input
-              type="password"
-              placeholder="Password"
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-4 border rounded-xl"
-            />
-
-            <button
-              type="submit"
-              className="w-full bg-black text-white py-4 rounded-xl"
-            >
-              Login
-            </button>
-          </form>
-        </div>
       </div>
-    </>
+
+    </div>
   );
 }
